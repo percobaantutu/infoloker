802dac82aca43b51a9b6f520cf1438d9e57e5a3f
percobaantutu <restumuhammad@upi.edu>
2025-11-14 20:12:16 +0700

    Finish LandingPage

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
+import Feature from "../../components/Feature";
+import Analytics from "../../components/Analytics";
+import Footer from "../../components/Footer";
 
 const LandingPage = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <Hero />
+      <Feature />
+      <Analytics />
+      <Footer />
     </div>
   );
 };
